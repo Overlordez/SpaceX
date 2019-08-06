cf3baefc2e1202d72603fdc286161e1938b8b3c5
overlord <dym8750@gmail.com>
2019-08-06 13:20:23 +0300

    другие картинки

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ function Footer(){
     return(
         <React.Fragment>
             <footer>
-
+            <a href="/">
+                <div className="logo">
+                <img className="logotype" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/UNESCO_logo_white.png/744px-UNESCO_logo_white.png" height="50" alt="..." /> 
+                </div>
+                </a>
                 <p> © 2019 UNESCO or its affiliated companies. All rights reserved. </p>
             </footer>
 
